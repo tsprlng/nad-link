07cc25989b62ddaf4185a815b4086f5fdd834781
Tom Spurling <tom@tspurling.co.uk>
2015-12-31 00:13:18 +0000

    Miracle code that fixes everything totally
    naaaaht

--- nad-link.py
+++ nad-link.py
@@ -29,50 +29,56 @@ cfg = yaml(file(os.path.join(os.path.dirname(__file__), 'config.yml'),'r'))
 mode, cmd = cfg[sys.argv[1]]
 hold = mode.lower() == 'hold'
 
-def usleep(us):
-  goal = monotonic() + (us * 0.000001)
-  while monotonic() < goal:
-    pass
-
 def pin(state):
   g.output(OutPin, state)
 
-def send(bit):
-  pin(False)
-  usleep(560)
-  pin(True)
-  usleep(1690 if bit else 560)
+def oneBitDelays(bit):
+  yield 560
+  yield (1690 if bit else 560)
 
-if not carryOn:
-  exit(0)
+def commandDelays(cmd):
+  if not carryOn:
+    exit(0)
 
-# Send preamble
-pin(False)
-usleep(9000)
-pin(True)
-usleep(4500)
+  # Send preamble
+  yield 9000
+  yield 4500
 
-# Send the remote code
-for c in "{0:b}".format(cmd):
-  if c=='1':
-    send(True)
-  elif c=='0':
-    send(False)
+  # Send the remote code one bit at a time
+  for c in "{0:b}".format(cmd):
+    for d in oneBitDelays(c=='1'):
+      yield d
 
-# Terminate the command
-pin(False)
-usleep(560)
-pin(True)
-usleep(42020)
+  # Terminate the command
+  yield 560
+  yield 42020
 
-# Send repeats until ^C in case of hold commands
-if hold:
-  while carryOn:
-    pin(False)
-    usleep(9000)
-    pin(True)
-    usleep(2250)
-    pin(False)
-    usleep(560)
-    pin(True)
-    usleep(98190)
+  # Send repeats until ^C in case of hold commands
+  if hold:
+    while carryOn:
+      yield 9000
+      yield 2250
+      yield 560
+      yield 98190
+
+def flippingInstants(cmd, firstInstant):
+  last = 0
+  for d in commandDelays(cmd):
+    if last > 0:
+      yield firstInstant + (last * 0.000001)
+    last += d
+
+
+currentState = True
+
+now = monotonic()
+currentState = not currentState
+pin(currentState)
+
+for i in flippingInstants(cmd, now):
+  while monotonic() < i:
+    pass
+  currentState = not currentState
+  pin(currentState)
+
+assert(currentState == True)  # we had an even number of delays and ended up where we started
