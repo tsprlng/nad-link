76761cfc5a599cea9509c524c0460ddbcf81d19c
Tom Spurling <tom@tspurling.co.uk>
2016-01-24 02:34:45 +0000

    Also include Hyperion commands (lighting control)

--- daemon.py
+++ daemon.py
@@ -4,25 +4,38 @@
 
 import struct
 import sys
-import subprocess
+from subprocess import Popen as popen
 import os.path
+import itertools
 
 CommandScript = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nad-link.py')
 def cmd(cmd):
-    return subprocess.Popen(['python', CommandScript, cmd])
+    return popen(['python', CommandScript, cmd])
 
 EventFilePath = "/dev/input/event1"  # n.b. this is probably not always the right one
 
 EventStructFormat = 'llHHI'  # long, long, unsigned short, unsigned short, unsigned int
 EventStructSize = struct.calcsize(EventStructFormat)
 
+HyperionSeqs = {
+    400: ['--color 222211', '--color 886633', '--color ffcc77', '--color 88ffcc'],
+    401: ['--color 000000', '--clearall']
+}
+hyperionLastKey = None
+hyperionSeq = None
+def hyperion(keycode):
+    global hyperionLastKey, hyperionSeq  # why must statics be so annoying?
+    if keycode != hyperionLastKey:
+        hyperionSeq = itertools.cycle(HyperionSeqs[keycode])
+        hyperionLastKey = keycode
+    popen(itertools.chain(['hyperion-remote'], hyperionSeq.next().split(' '))).wait()
 
 thing = None  # holds the current invocation of the nad-link script (while there is one) for holdy commands like volume
 
 def handle(type, code, value):
     global thing
 
-    if (code < 113 or code > 115) and (code < 398 or code > 399):  # disregard bizarre noise events
+    if (code < 113 or code > 115) and (code < 398 or code > 401):  # disregard bizarre noise events
         return
 
     if thing:
@@ -44,6 +57,8 @@ def handle(type, code, value):
         cmd('off').wait()
     elif code == 399:  # green button
         cmd('on').wait()
+    elif code >= 400 and code <= 401:  # yellow and blue!
+        hyperion(code)
 
 
 if len(sys.argv) >= 2 and sys.argv[1] == 'print':  # i miss 'rescue nil'
