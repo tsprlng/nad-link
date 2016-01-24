7fc4805f3ccb64ff8f6ff290499b064702d92b7d
Tom Spurling <tom@tspurling.co.uk>
2016-01-24 01:02:34 +0000

    Tidy up and comment things a bit

--- nad-link.py
+++ nad-link.py
@@ -1,30 +1,5 @@
-import signal
-
-# Catch ^C (SIGINT) and handle politely
-carryOn = True
-def sigint_handler(signal, frame):
-  global carryOn
-  carryOn = False
-signal.signal(signal.SIGINT, sigint_handler)
-
-import RPi.GPIO as g
-import time
-import sys
-import os
-import atexit
-
-from monotonic import monotonic
-
 OutPin = 7
-
-def cleanup():
-  g.cleanup(OutPin)
-atexit.register(cleanup)
-
-g.setmode(g.BOARD)
-g.setup(OutPin, g.OUT, initial=True)
-
-cfg = {  # This configuration is for my C350
+Config = {  # This configuration is for my C350
 
   'off':    ('once', 0xe13e13ec),
   'on':     ('once', 0xe13ea45b),
@@ -49,27 +24,59 @@ cfg = {  # This configuration is for my C350
   'src-':     ('once', 0xe13eb847),
   'src+':     ('once', 0xe13e58a7),
 
-  # Personal source aliases
+  # Personal source aliases :D
   #
-  'rad':      ('once', 0xe13ebb44),
+  'radio':    ('once', 0xe13ebb44),
   'sofa':     ('once', 0xe13ed926),
   'pi':       ('once', 0xe13e43bc),
   'mac':      ('once', 0xe13ea15e)
 }
 
-mode, cmd = cfg[sys.argv[1]]
+
+# Catch ^C (SIGINT) and handle politely
+#
+carryOn = True  # yayyy global
+#
+import signal
+def sigint_handler(signal, frame):
+  global carryOn
+  carryOn = False
+signal.signal(signal.SIGINT, sigint_handler)
+
+
+import RPi.GPIO as g
+import time
+import sys
+import os
+import atexit
+
+from monotonic import monotonic
+
+
+def cleanup():
+  g.cleanup(OutPin)
+atexit.register(cleanup)
+
+g.setmode(g.BOARD)
+g.setup(OutPin, g.OUT, initial=True)
+
+
+mode, cmd = Config[sys.argv[1]]
 hold = mode.lower() == 'hold'
 
-def pin(state):
-  g.output(OutPin, state)
 
+# The magic durations between output state flips that represent a single remote-control signal bit in NAD-land
+#
 def oneBitDelays(bit):
   yield 560
   yield (1690 if bit else 560)
 
+
+# The magic durations between output state flips for an entire command.
+#
 def commandDelays(cmd):
   if not carryOn:
-    exit(0)
+    exit(0)  # let us quit before we even begin!
 
   # Send preamble
   yield 9000
@@ -84,7 +91,8 @@ def commandDelays(cmd):
   yield 560
   yield 42020
 
-  # Send repeats until ^C in case of hold commands
+  # Send repeats until ^C in case of hold commands.
+  # I love that Python generators are properly lazy and therefore this disgusting use of shared 'carryOn' state actually works.
   if hold:
     while carryOn:
       yield 9000
@@ -92,6 +100,12 @@ def commandDelays(cmd):
       yield 560
       yield 98190
 
+
+# Transformation of the 'between flip durations' iterator into an iterator over actual absolute moments in time.
+# This an attempt to improve timing precision by stopping delay errors accumulating, calculating all delays relative to the start of the event.
+# As I don't have an oscilloscope or anything like that, I cannot really tell whether it works.
+# Subjectively, this script seems to successfully trigger the amp more often than the last version...
+#
 def flippingInstants(cmd, firstInstant):
   last = 0
   for d in commandDelays(cmd):
@@ -104,12 +118,12 @@ currentState = True
 
 now = monotonic()
 currentState = not currentState
-pin(currentState)
+g.output(OutPin, currentState)
 
 for i in flippingInstants(cmd, now):
   while monotonic() < i:
     pass
   currentState = not currentState
-  pin(currentState)
+  g.output(OutPin, currentState)
 
-assert(currentState == True)  # we had an even number of delays and ended up where we started
+assert(currentState == True)  # We had an even number of output flips and ended up where we started (I didn't forget a yield. (Perhaps I forgot two though.))
